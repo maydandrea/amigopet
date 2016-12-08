2384f543a6bf70f0fba805d0ab7b8d8cd3a9afad
Kelly <kellycristina.ps@hotmail.com>
2016-12-08 00:51:54 +0000

    Modificacao login

--- Login.hs
+++ Login.hs
@@ -7,3 +7,14 @@ import Yesod
 import Database.Persist.Postgresql
 import Data.Text
 
+postLoginR :: Handler ()
+postLoginR = do
+    log <- requireJsonBody :: Handler Login
+    lid <- runDB $ insert log
+    sendResponse (object [pack "resp" .= pack ("CREATED " ++ (show $ fromSqlKey lid))])
+
+{-getLoginR :: LoginId -> Handler ()
+getLoginR lid = do
+    log <- runDB $ get404 lid
+    sendResponse (object [pack "email" .= loginEmail log,
+                          pack  "senha" .= loginSenha log])-}
